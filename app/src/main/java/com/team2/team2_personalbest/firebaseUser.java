5a741e7d54aef3e894177dc91257b2c0a9abb337
Daniel <dfritsch99@gmail.com>
2019-03-09 13:00:05 -0800

    Added some functionality for error cases and then tests.

--- app/src/main/java/com/team2/team2_personalbest/firebaseUser.java
+++ app/src/main/java/com/team2/team2_personalbest/firebaseUser.java
@@ -21,6 +21,7 @@ public class FirebaseUser extends IUser {
     final String DATABASE_NAME = "days_db";
     private String USER_NAME = "dev";
 
+
     public FirebaseUser(Context activityContext){
         dayDatabase = Room.databaseBuilder(activityContext,
                 DayDatabase.class, DATABASE_NAME)
