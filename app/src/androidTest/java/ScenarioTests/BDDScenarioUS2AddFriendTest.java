b2c35f6fa209d94abe0a38aff0416749cacd8cc5
Joseph Li <joseph.310.li@gmail.com>
2019-03-15 14:05:29 -0700

    Created MockFirestoreUser

---
+++ app/src/androidTest/java/ScenarioTests/BDDScenarioUS2AddFriendTest.java
@@ -0,0 +1,24 @@
+package ScenarioTests;
+
+import com.team2.team2_personalbest.TestUser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class BDDScenarioUS2AddFriendTest {
+
+    public HashMap<Integer, TestUser> users = new HashMap<>();
+
+    @Before
+    private void setUpFriends(){
+        users.put(1, new TestUser("Joey", 1));
+        users.put(2, new TestUser("Chandler", 2));
+    }
+
+    @Test
+    private void addFriends(){
+
+    }
+}
---
+++ app/src/main/java/com/team2/team2_personalbest/MockFirestoreUser.java
@@ -0,0 +1,10 @@
+package com.team2.team2_personalbest;
+
+public class MockFirestoreUser extends FirestoreUser {
+
+    protected Friend User;
+
+    public MockFirestoreUser (String name, String email){
+
+    }
+}
