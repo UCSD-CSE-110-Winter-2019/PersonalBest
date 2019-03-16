74d49777bd183467e22cd97bc3ff5d145f04c67b
Daniel <dfritsch99@gmail.com>
2019-03-15 20:48:38 -0700

    FriendGraph tested

--- app/src/androidTest/java/com/team2/team2_personalbest/TestFriendGraph.java
+++ app/src/androidTest/java/com/team2/team2_personalbest/TestFriendGraph.java
@@ -69,7 +69,7 @@ public class TestFriendGraph {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(5000);
+            Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
